7c45db3d5d36b4e0f5c9cc51ae5217e2a81a17dc
Daniyal Hasnain <hasnaind@myumanitoba.ca>
2025-11-10 19:24:56 -0600

    chore: add icons for temperature control buttons

--- app/constants/icons.js
+++ app/constants/icons.js
@@ -1,13 +1,21 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
+import { Feather } from "@expo/vector-icons";
+
 const icons = {
-    control: (color, size) => (
-        <FontAwesome5 name="temperature-high" size={24} color="black" />
-    ),
-    trends: (color, size) => (
-        <SimpleLineIcons name="graph" size={24} color="black" />
-    )
-}
+  control: (colour = "black", size = 24) => (
+    <FontAwesome5 name="temperature-high" size={size} color={colour} />
+  ),
+  trends: (colour = "black", size = 24) => (
+    <SimpleLineIcons name="graph" size={size} color={colour} />
+  ),
+
+  plus: (colour = "black", size = 24) => (
+    <Feather name="plus" size={size} color={colour} />
+  ),
+  minus: (colour = "black", size = 24) => (
+    <Feather name="minus" size={size} color={colour} />
+  ),
+};
 
 export default icons;
